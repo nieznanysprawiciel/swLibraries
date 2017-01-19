6634eedd6c3d2374452075454e764a6e6baa7ac1
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2017-01-19 23:38:16 +0100

    Moves in progress

---
+++ Serialization/PropertySerialization/EngineSerializationContext.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CommonLibrary/Common/RTTR.h"
+#include "CommonLibrary/Serialization/SerializationContext.h"
+
+#include <map>
+#include <vector>
+
+
+
+struct EngineSerializationContext : public SerializationContext
+{
+	typedef std::map< rttr::type, std::vector< rttr::property > > TypePropertyMap;
+public:
+
+	bool					SaveWholeMap;		///< Wartoœæ true oznacza, ¿e zapisujemy mapê, false, ¿e zapisujemy stan gry.
+	bool					EditorLoad;			///< Wczytywanie mapy w trybie edytora.
+
+	TypePropertyMap			TypeProperties;		///< Mapa zawieraj¹ca typy i ich w³aœciwoœci. W³aœciwoœci nieserializowane s¹ odfiltrowane.
+
+
+
+public:
+	virtual		~EngineSerializationContext() = default;
+};
+
