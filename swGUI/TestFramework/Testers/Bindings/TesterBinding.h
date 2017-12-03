f5c0156e279fc8bc80fb735e534ef7b8fdd3df78
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2017-12-03 22:31:33 +0100

    [Test] Binding with converters test

--- swGUI/TestFramework/Testers/Bindings/TesterBinding.h
+++ swGUI/TestFramework/Testers/Bindings/TesterBinding.h
@@ -2,6 +2,8 @@
 
 #include "swGUI/Core/System/DataBinding/Binding.h"
 
+#include "swGUI/Core/System/DataBinding/Converters/IValueConverter.h"
+
 
 namespace sw {
 namespace gui
@@ -13,13 +15,49 @@ class CLASS_TESTER( Binding )
 {
 protected:
 
+	// ================================ //
+	//
 	Nullable< void >	ValidateBinding			( Binding& binding, TypeID srcType, TypeID targetType )
 	{
 		return binding.ValidateBinding( srcType, targetType );
 	}
 
+	// ================================ //
+	//
+	void				SetConverter			( Binding& binding, IValueConverter* converter )
+	{
+		binding.SetConverter( converter );
+	}
 };
 
+
+// ================================ //
+//
+class MockConverter : public IValueConverter
+{
+private:
+
+	bool		m_forwardConversion;
+	bool		m_backwardConversion;
+
+public:
+
+	void		SetConversionResult		( bool forward, bool backward )
+	{
+		m_forwardConversion = forward;
+		m_backwardConversion = backward;
+	}
+
+
+public:
+
+	virtual rttr::variant	Convert				( const rttr::variant& object, TypeID targetType, const rttr::variant& params, const CultureInfo* culture ) const override;
+	virtual rttr::variant	ConvertBack			( const rttr::variant& object, TypeID targetType, const rttr::variant& params, const CultureInfo* culture ) const override;
+	
+	virtual bool			CanConvert			( TypeID srcType, TypeID targetType ) const override;
+	virtual bool			CanConvertBack		( TypeID srcType, TypeID targetType ) const override;
+
+};
 
 }	// gui
 }	// sw
--- swGUI/Tests/TestBindings/TestBindings/TestBindingCompatibility.cpp
+++ swGUI/Tests/TestBindings/TestBindings/TestBindingCompatibility.cpp
@@ -104,3 +104,70 @@ TEST_CASE_METHOD( gui::CLASS_TESTER( Binding ), "Binding_Compatibility", "[GUI][
 		CHECK_FALSE( ValidateBinding( binding, TypeID::get< Dog* >(), TypeID::get< uint32 >() ).IsValid() );
 	}
 }
+
+
+// ================================ //
+// Checks bound types compatibility with converter set.
+TEST_CASE_METHOD( gui::CLASS_TESTER( Binding ), "Binding_Compatibility_Converter", "[GUI][BindingSystem]" )
+{
+	gui::Binding binding( nullptr, nullptr, Properties::EmptyProperty() );
+	std::unique_ptr< gui::MockConverter > converter = std::make_unique< gui::MockConverter >();
+
+	SetConverter( binding, converter.get() );
+
+	SECTION( "ToTargetConversion" )
+	{
+		binding.SetBindingMode( gui::BindingMode::OneWay );
+		converter->SetConversionResult( true, false );
+		CHECK( ValidateBinding( binding, TypeID::get< uint32 >(), TypeID::get< uint32 >() ).IsValid() );
+	}
+
+	SECTION( "ToSourceConversion" )
+	{
+		binding.SetBindingMode( gui::BindingMode::OneWayToSource );
+		converter->SetConversionResult( false, true );
+		CHECK( ValidateBinding( binding, TypeID::get< uint32 >(), TypeID::get< uint32 >() ).IsValid() );
+	}
+
+	SECTION( "TwoWayConversion" )
+	{
+		binding.SetBindingMode( gui::BindingMode::TwoWay );
+		converter->SetConversionResult( true, true );
+		CHECK( ValidateBinding( binding, TypeID::get< uint32 >(), TypeID::get< uint32 >() ).IsValid() );
+	}
+
+	// ================================ //
+	// Converter should convert value in binding mode direction. Otherwise whole conversion fails.
+	SECTION( "InvalidToSourceConversion" )
+	{
+		binding.SetBindingMode( gui::BindingMode::OneWayToSource );
+		converter->SetConversionResult( true, false );
+		CHECK_FALSE( ValidateBinding( binding, TypeID::get< uint32 >(), TypeID::get< uint32 >() ).IsValid() );
+	}
+
+	SECTION( "InvalidToTargetConversion" )
+	{
+		binding.SetBindingMode( gui::BindingMode::OneWay );
+		converter->SetConversionResult( false, false );
+		CHECK_FALSE( ValidateBinding( binding, TypeID::get< uint32 >(), TypeID::get< uint32 >() ).IsValid() );
+	}
+
+	// ================================ //
+	// One invalid conversion is means that whole conversion fails.
+	SECTION( "TwoWay_OneInvalidConversion" )
+	{
+		binding.SetBindingMode( gui::BindingMode::TwoWay );
+		converter->SetConversionResult( true, false );
+		CHECK_FALSE( ValidateBinding( binding, TypeID::get< uint32 >(), TypeID::get< uint32 >() ).IsValid() );
+
+		converter->SetConversionResult( false, true );
+		CHECK_FALSE( ValidateBinding( binding, TypeID::get< uint32 >(), TypeID::get< uint32 >() ).IsValid() );
+	}
+
+	SECTION( "TwoWay_BothInvalidConversions" )
+	{
+		binding.SetBindingMode( gui::BindingMode::TwoWay );
+		converter->SetConversionResult( false, false );
+		CHECK_FALSE( ValidateBinding( binding, TypeID::get< uint32 >(), TypeID::get< uint32 >() ).IsValid() );
+	}
+}
