61ce3cca6b1b15f5c5657ef23725905a94672872
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-01-04 20:10:06 +0100

    [Test] Test RenderingSystem initialization using DX11

---
+++ swGUI/Tests/TestRenderingSystem.DX11/TestRenderingSystem.cpp
@@ -0,0 +1,28 @@
+#include "swCommonLib/External/Catch/catch.hpp"
+
+#include "swGUI/TestFramework/DX11/TestFramework.DX11.h"
+#include "swGUI/TestFramework/Testers/Rendering/RenderingSystemTester.h"
+
+
+using namespace sw;
+using namespace sw::gui;
+
+
+
+// ================================ //
+//
+TEST_CASE( "GUI.RenderingSystem.Initialization", "[GUISystem][RenderingSystem]" )
+{
+	TestFramework* framework = GetGlobalTestFramework();
+	auto renderingSystem = framework->GetRenderingSystem();
+
+	CHECK( renderingSystem->GetRenderer() != nullptr );
+	CHECK( renderingSystem->GetShaderProvider() != nullptr );
+
+	CHECK( CLASS_TESTER( RenderingSystem )::GetRasterizerState( renderingSystem ) != nullptr );
+	CHECK( CLASS_TESTER( RenderingSystem )::GetDepthStencilState( renderingSystem ) != nullptr );
+	CHECK( CLASS_TESTER( RenderingSystem )::GetTransparentBlendingState( renderingSystem ) != nullptr );
+	CHECK( CLASS_TESTER( RenderingSystem )::GetOpaqueBlendingState( renderingSystem ) != nullptr );
+}
+
+
