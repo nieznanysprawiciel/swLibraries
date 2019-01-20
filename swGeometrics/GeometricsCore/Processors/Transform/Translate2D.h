b92bf741998f12341fc697ca5ce06a24d87b1c02
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-01-20 16:50:13 +0100

    Translate2D processor added

---
+++ swGeometrics/GeometricsCore/Processors/Transform/Translate2D.h
@@ -0,0 +1,96 @@
+#pragma once
+/**
+@file Translate2D.h
+@author nieznanysprawiciel
+@copyright File is part of Sleeping Wombat Libraries.
+*/
+
+#include "swCommonLib/Common/TypesDefinitions.h"
+#include "swCommonLib/Common/Converters.h"
+#include "swCommonLib/Common/Exceptions/ErrorsCollector.h"
+
+#include "swGeometrics/GeometricsCore/Types/Traits/ProcessorTraits.h"
+#include "swGeometrics/GeometricsCore/Types/Accessors/DefaultAccessors.h"
+
+
+namespace sw {
+namespace geom
+{
+
+
+/**@brief Generated planar UV mapping.
+@ingroup Coordinates*/
+template< typename VertexType, typename PositionAcc = DefaultPositionAcc< VertexType > >
+class Translate2D : public ProcessorTraits< VertexType >
+{
+private:
+public:
+
+	float				TranslateX;
+	float				TranslateY;
+
+public:
+
+	inline explicit		Translate2D				();
+	inline explicit		Translate2D				( float x, float y );
+
+	inline void			GenerateVertex			( VertexType& vertex, Size vertexIdx );
+
+
+	ReturnResult		ValidateParams			() const;
+};
+
+//====================================================================================//
+//			Implementation	
+//====================================================================================//
+
+
+// ================================ //
+//
+template< typename VertexType, typename PositionAcc >
+inline						Translate2D< VertexType, PositionAcc >::Translate2D				()
+	:	TranslateX( 0.0f )
+	,	TranslateY( 0.0f )
+{}
+
+// ================================ //
+//
+template< typename VertexType, typename PositionAcc >
+inline						Translate2D< VertexType, PositionAcc >::Translate2D				( float x, float y )
+	:	TranslateX( x )
+	,	TranslateY( y )
+{}
+
+// ================================ //
+//
+template< typename VertexType, typename PositionAcc >
+inline void					Translate2D< VertexType, PositionAcc >::GenerateVertex			( VertexType& vertex, Size )
+{
+	auto& vertexPos = PositionAcc::Get( vertex );
+
+	vertexPos.x += TranslateX;
+	vertexPos.y += TranslateY;
+}
+
+// ================================ //
+//
+template< typename VertexType, typename PositionAcc >
+inline ReturnResult			Translate2D< VertexType, PositionAcc >::ValidateParams			() const
+{
+	ErrorsCollector collector;
+
+	if( isfinite( TranslateX ) )
+		collector.Add( "[Translate2D] Invalid parameter [TranslateX=" + Convert::ToString( TranslateX ) + "].");
+
+	if( isfinite( TranslateY ) )
+		collector.Add( "[Translate2D] Invalid parameter [TranslateY=" + Convert::ToString( TranslateY ) + "].");
+
+	return collector;
+}
+
+
+
+
+}	// geom
+}	// sw
+
---
+++ swGeometrics/Tests/TestGenerationFramework/TestTransforms.cpp
@@ -0,0 +1,45 @@
+#include "swCommonLib/External/Catch/catch.hpp"
+
+
+#include "swGeometrics/GeometricsCore/Generators/Generator.h"
+#include "swGeometrics/GeometricsCore/Types/VertexLayouts/VertexShape2D.h"
+#include "swGeometrics/GeometricsCore/Types/IndexTypes.h"
+
+#include "swGeometrics/GeometricsCore/Processors/Transform/Translate2D.h"
+
+#include "swGeometrics/BasicShapes/Shapes2D/Rectangle.h"
+
+
+using namespace sw;
+using namespace sw::geom;
+
+
+
+// ================================ //
+//
+TEST_CASE( "Geometrics.Transforms.Translate2D.TranslateRect", "[Geometrics][Transform]" )
+{
+	Rectangle< VertexShape2D, Index16 > rect;
+	rect.TopLeftX = 30;
+	rect.TopLeftY = 30;
+	rect.Height = 20;
+	rect.Width = 20;
+
+	auto geometry = Generate< IndexedGeometry< VertexShape2D, Index16 > >( rect, Translate2D< VertexShape2D >( 3.5f, 5.3f ) ).Get();
+	
+	// Check vertex buffer.
+	CHECK( geometry.Verticies.size() == 4 );
+
+	CHECK( geometry.Verticies[ 0 ].Position.x == 33.5f );
+	CHECK( geometry.Verticies[ 0 ].Position.y == 35.3f );
+
+	CHECK( geometry.Verticies[ 1 ].Position.x == 53.5f );
+	CHECK( geometry.Verticies[ 1 ].Position.y == 35.3f );
+
+	CHECK( geometry.Verticies[ 2 ].Position.x == 33.5f );
+	CHECK( geometry.Verticies[ 2 ].Position.y == 15.3f );
+
+	CHECK( geometry.Verticies[ 3 ].Position.x == 53.5f );
+	CHECK( geometry.Verticies[ 3 ].Position.y == 15.3f );
+}
+
