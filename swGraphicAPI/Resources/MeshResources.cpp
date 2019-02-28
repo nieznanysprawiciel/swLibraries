056e917ae6af24932fa1b244075dbbdd12708a0c
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-02-28 21:31:40 +0100

    Deprecated MaterialObject removed

--- swGraphicAPI/Resources/MeshResources.cpp
+++ swGraphicAPI/Resources/MeshResources.cpp
@@ -309,12 +309,6 @@ RTTR_REGISTRATION
 		.property( "DepthBuffer", &RenderTargetObject::m_depthBuffer )
 		.property( "StencilBuffer", &RenderTargetObject::m_stencilBuffer );
 
-	rttr::registration::class_< MaterialObject >( "MaterialObject" )
-		.property( "Diffuse", &MaterialObject::Diffuse ) BIND_AS_PTR
-		.property( "Specular", &MaterialObject::Specular ) BIND_AS_PTR
-		.property( "Ambient", &MaterialObject::Ambient ) BIND_AS_PTR
-		.property( "Emissive", &MaterialObject::Emissive ) BIND_AS_PTR
-		.property( "SpecularPower", &MaterialObject::Power );
 
 }
 
@@ -396,44 +390,4 @@ BufferObject::BufferObject( unsigned int elementSize, unsigned int elementCount 
 //
 TypeID		BufferInitData::GetAssetType	() const
 {	return TypeID::get< BufferObject >(); 	}
-
-//----------------------------------------------------------------------------------------------//
-//								MaterialObject													//
-//----------------------------------------------------------------------------------------------//
-
-MaterialObject::MaterialObject( const MaterialObject* material )
-: Resource(WRONG_ID)
-{
-	memcpy( this, material, sizeof(MaterialObject) );
-}
-
-/**@brief Ustawia materia³ na wartoœci domyœlne dla silnika.
-
-Te wartoœci s¹ najbardziej neutralne, w przypadku gdy nie ma materia³u, a jest ustawiona tekstura.
-Wtedy shadery wymana¿aj¹ jasnoœæ piksela przez 1.0 i nic sie nie zmienia.*/
-void MaterialObject::SetNullMaterial( )
-{
-	Diffuse.x = 1.0f;
-	Diffuse.y = 1.0f;
-	Diffuse.z = 1.0f;
-	Diffuse.w = 1.0f;
-
-	Ambient.x = 1.0f;
-	Ambient.y = 1.0f;
-	Ambient.z = 1.0f;
-	Ambient.w = 1.0f;
-
-	Specular.x = 1.0f;
-	Specular.y = 1.0f;
-	Specular.z = 1.0f;
-	Specular.w = 1.0f;
-
-	Emissive.x = 0.0f;
-	Emissive.y = 0.0f;
-	Emissive.z = 0.0f;
-	Emissive.w = 0.0f;
-
-	Power = 1.0f;
-}
-
 
