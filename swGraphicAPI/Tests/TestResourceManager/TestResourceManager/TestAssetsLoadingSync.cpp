753b576fd2a2b53f57388e578cdcbd220b030767
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-05-19 18:19:01 +0200

    [Test] Load asset from two threads at the same time

--- swGraphicAPI/Tests/TestResourceManager/TestResourceManager/TestAssetsLoadingSync.cpp
+++ swGraphicAPI/Tests/TestResourceManager/TestResourceManager/TestAssetsLoadingSync.cpp
@@ -16,6 +16,8 @@
 
 #include "swGraphicAPI/Tests/TestResourceManager/Utils/Utils.h"
 
+#include "swCommonLib/Common/Multithreading/ThreadsBarrier.h"
+
 
 using namespace sw;
 
@@ -68,5 +70,35 @@ TEST_CASE( "GraphicAPI.ResourceManager.LoadGeneric.NotRegisteredLoader", "[Graph
 
 	// This will hang if assets locks aren't released properly.
 	auto resource2 = rm->LoadGeneric( "../TestAssets/mock/example.mock", &info, TypeID::get< MockAsset >() );
+}
+
+// ================================ //
+// Load resource from two threads at the same time. Only one resource should
+// be loaded.
+TEST_CASE( "GraphicAPI.ResourceManager.LoadGeneric.TwoThreadsSameAsset", "[GraphicAPI]" )
+{
+	auto rm = CreateResourceManagerWithMocks();
+
+	MockAssetLoadInfo info;
+	ThreadsBarrier barrier( 2 );
+
+	Nullable< ResourcePointer > threadResource;
+
+	std::thread thread( [ & ]()
+	{
+		barrier.ArriveAndWait();
+		threadResource = rm->LoadGeneric( "../TestAssets/mock/example.mock", &info, TypeID::get< MockAsset >() );
+	} );
+
+	// Wait for second thread initialization.
+	barrier.ArriveAndWait();
+	auto resource = rm->LoadGeneric( "../TestAssets/mock/example.mock", &info, TypeID::get< MockAsset >() );
+
+	thread.join();
+
+	REQUIRE( resource.IsValid() == true );
+	REQUIRE( threadResource.IsValid() == true );
+
+	CHECK( resource.Get() == threadResource.Get() );
 }
 
