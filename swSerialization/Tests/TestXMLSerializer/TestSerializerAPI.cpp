ecb74fe71caff7112a2c294eafdf5956c807a4c5
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-11-29 22:28:57 +0100

    [Test] Tree structure test for SerializerXML

--- swSerialization/Tests/TestXMLSerializer/TestSerializerAPI.cpp
+++ swSerialization/Tests/TestXMLSerializer/TestSerializerAPI.cpp
@@ -39,13 +39,62 @@ TEST_CASE( "Serializer.XML.SerialObject.AddObject", "[Serializers][SerializerXML
     }
 }
 
+// ================================ //
+//
+TEST_CASE( "Serializer.XML.SerialObject.AddObject.TreeStructure", "[Serializers][SerializerXML]" )
+{
+    {
+        SerializerXML ser( std::make_unique< ISerializationContext >() );
+        SerialObject root = ser.Root();
 
-//firstObject.AddAttribute( "Map", "LightmapGen1" );
-//firstObject.AddAttribute( "Path", "/LightmapGen1.map" );
-//firstObject.AddAttribute( "Load", true );
-//firstObject.AddAttribute( "Number", 32 );
-//firstObject.AddAttribute( "Floating", 32.4362 );
-//firstObject.AddAttribute( "Type", 'g' );
+        auto firstObject = root.AddObject( "FirstObject" );
 
+        auto first = firstObject.AddObject( "First" );
+        auto second = firstObject.AddObject( "Second" );
+        auto third = firstObject.AddObject( "Third" );
 
+        // Change order of adding elements.
+        third.AddObject( "Child1" );
+        second.AddObject( "Child2" );
+        first.AddObject( "Child3" );
 
+        auto saveResult = ser.SaveFile( "SerializerTest/XML/Generated/Test-AddObject-TreeStructure.xml", sw::WritingMode::Readable );
+        REQUIRE_IS_VALID( saveResult );
+    }
+
+    {
+        SerializerXML deser( std::make_unique< ISerializationContext >() );
+        auto loadResult = deser.LoadFromFile( "SerializerTest/XML/Generated/Test-AddObject-TreeStructure.xml" );
+        REQUIRE_IS_VALID( loadResult );
+
+        SerialObject root = deser.Root();
+        REQUIRE( root.GetElement( "FirstObject" ).has_value() );
+        REQUIRE( root.GetElement( "FirstObject" ).value().IsObject() );
+
+        auto firstObject = root.GetElement( "FirstObject" ).value().ObjectView().value();
+
+        REQUIRE( firstObject.GetElement( "First" ).has_value() );
+        REQUIRE( firstObject.GetElement( "First" ).value().IsObject() );
+
+        REQUIRE( firstObject.GetElement( "Second" ).has_value() );
+        REQUIRE( firstObject.GetElement( "Second" ).value().IsObject() );
+
+        REQUIRE( firstObject.GetElement( "Third" ).has_value() );
+        REQUIRE( firstObject.GetElement( "Third" ).value().IsObject() );
+
+        auto third = firstObject.GetElement( "Third" ).value().ObjectView().value();
+
+        CHECK( third.GetElement( "Child1" ).has_value() );
+        CHECK( third.GetElement( "Child1" ).value().IsObject() );
+
+        auto second = firstObject.GetElement( "Second" ).value().ObjectView().value();
+
+        CHECK( second.GetElement( "Child2" ).has_value() );
+        CHECK( second.GetElement( "Child2" ).value().IsObject() );
+
+        auto first = firstObject.GetElement( "First" ).value().ObjectView().value();
+
+        CHECK( first.GetElement( "Child3" ).has_value() );
+        CHECK( first.GetElement( "Child3" ).value().IsObject() );
+    }
+}
