2f5bea8b66a799d9177f1fd1204a9029192f7dd6
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-12-12 17:39:43 +0100

    CanConvert template (not implemented)

---
+++ swCommonLib/Common/Converters/CanConvert.h
@@ -0,0 +1,31 @@
+#pragma once
+/**
+@file CanConvert.h
+@author nieznanysprawiciel
+@copyright File is part of Sleeping Wombat Libraries.
+*/
+
+
+#include <type_traits>
+
+
+namespace sw
+{
+
+
+/**@brief Checks if conversion is possible.
+Functions take into account not only types, but variables range.*/
+class CanConvert
+{
+
+    //template< typename SrcType, typename DstType >
+    //static inline sw::Nullable< typename std::enable_if< std::is_arithmetic< SrcType >::value
+    //                                                     std::is_arithmetic< DstType >::value, DstType >::type >
+    //                            To			( const SrcType& value );
+};
+
+
+}	// sw
+
+
+
