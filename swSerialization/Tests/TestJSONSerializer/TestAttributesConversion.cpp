eb4f19aed767a31859d4e48f1eb29fe36cd404e6
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-12-26 16:49:16 +0100

    [Test] Test negative int conversions

--- swSerialization/Tests/TestJSONSerializer/TestAttributesConversion.cpp
+++ swSerialization/Tests/TestJSONSerializer/TestAttributesConversion.cpp
@@ -62,6 +62,52 @@ TEST_CASE( "Serializer.JSON.SerialAttribute.Conversions.Int", "[Serializers][Ser
 
 // ================================ //
 //
+TEST_CASE( "Serializer.JSON.SerialAttribute.Conversions.Int.Negative", "[Serializers][SerializerJSON]" )
+{
+    {
+        SerializerJSON ser( std::make_unique< ISerializationContext >() );
+        SerialObject root = ser.Root();
+
+        root.AddAttribute( "Value", -15 );
+
+        REQUIRE_IS_VALID( ser.SaveFile( "SerializerTest/JSON/Generated/Test-Attribute-Conversions-Int-Negative.json", sw::WritingMode::Readable ) );
+    }
+
+    {
+        SerializerJSON deser( std::make_unique< ISerializationContext >() );
+        REQUIRE_IS_VALID( deser.LoadFromFile( "SerializerTest/JSON/Generated/Test-Attribute-Conversions-Int-Negative.json" ) );
+
+        SerialObject root = deser.Root();
+
+        REQUIRE( root.GetElement( "Value" ).has_value() == true );
+        REQUIRE( root.GetElement( "Value" ).value().IsAttribute() );
+
+        auto valueAttribute = root.GetElement( "Value" ).value().AttributeView().value();
+
+        REQUIRE( valueAttribute.GetType() == SerialType::Int64 );
+
+        // Check other conversions.
+        CHECK( valueAttribute.ConvertToInt64().has_value() == true );
+        CHECK( valueAttribute.ConvertToInt32().has_value() == true );
+        CHECK( valueAttribute.ConvertToInt16().has_value() == true );
+        CHECK( valueAttribute.ConvertToInt8().has_value() == true );
+        CHECK( valueAttribute.ConvertToDouble().has_value() == true );
+
+        CHECK( valueAttribute.ConvertToUInt64().has_value() == false );
+        CHECK( valueAttribute.ConvertToUInt32().has_value() == false );
+        CHECK( valueAttribute.ConvertToUInt16().has_value() == false );
+        CHECK( valueAttribute.ConvertToUInt8().has_value() == false );
+
+        CHECK( valueAttribute.ConvertToString().has_value() == false );
+        CHECK( valueAttribute.ConvertToBool().has_value() == false );
+        CHECK( valueAttribute.ConvertToChar().has_value() == false );
+
+        CHECK( valueAttribute.ConvertToInt64().value() == -15 );
+    }
+}
+
+// ================================ //
+//
 TEST_CASE( "Serializer.JSON.SerialAttribute.Conversions.Double", "[Serializers][SerializerJSON]" )
 {
     {
