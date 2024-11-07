0c0e530eecf50ec2b75417e6cde8ad7f8a623521
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2024-11-08 00:30:05 +0100

    Test newlines and spaces

--- swGraphicAPI/Tests/TestText/TestArranger.cpp
+++ swGraphicAPI/Tests/TestText/TestArranger.cpp
@@ -22,7 +22,7 @@ using namespace sw;
 
 // ================================ //
 // 
-TEST_CASE( "GraphicAPI.Text.Generate.Buffers", "[GraphicAPI][Text]" )
+TEST_CASE( "GraphicAPI.Text.Generate.TrickyTexts.SpacesAndNewlines", "[GraphicAPI][Text]" )
 {
     auto rm = CreateResourceManagerWithFonts();
     auto api = ResourceManagerAPI( rm.get() );
@@ -33,6 +33,15 @@ TEST_CASE( "GraphicAPI.Text.Generate.Buffers", "[GraphicAPI][Text]" )
     REQUIRE_IS_VALID( font );
 
     TextArranger arranger;
-    auto geometry = arranger.GenerateGeometry( L"Hello World", font.Get(), false );
-    REQUIRE_IS_VALID( geometry );
+    auto geometry1 = arranger.GenerateGeometry( L"Hello World", font.Get(), false );
+    REQUIRE_IS_VALID( geometry1 );
+
+    auto geometry2 = arranger.GenerateGeometry( L"Hello World\n", font.Get(), false );
+    REQUIRE_IS_VALID( geometry2 );
+
+    auto geometry3 = arranger.GenerateGeometry( L"   Hello  World   ", font.Get(), false );
+    REQUIRE_IS_VALID( geometry3 );
+
+    auto geometry4 = arranger.GenerateGeometry( L"Hello World\n\n\n\n", font.Get(), false );
+    REQUIRE_IS_VALID( geometry4 );
 }
